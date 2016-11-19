0e6451bea2d9eb89928f8226aaad5d156774966e
Davide Del Vento <davide.del.vento@gmail>
2016-11-19 14:02:20 -0700

    refactoring the test using a fixture

--- test_temperanotes.py
+++ test_temperanotes.py
@@ -1,7 +1,13 @@
 import temperanotes
-def test_two_octaves():
-    idiot_temp = [1, 1.05, 1.1, 1.15, 1.2, 1.3, 1.4, 1.5, 1.6, 1.7, 1.8, 1.9]            # not a temperament, just a set of numbers for testing
-    assert len(idiot_temp) == 12                                                         # need 12 notes for the chromatic scale
+import pytest
+
+@pytest.fixture
+def idiot_temp():
+    temp = [1, 1.05, 1.1, 1.15, 1.2, 1.3, 1.4, 1.5, 1.6, 1.7, 1.8, 1.9]            # not a temperament, just a set of numbers for testing
+    assert len(temp) == 12                                                         # need 12 notes for the chromatic scale
+    return temp
+
+def test_two_octaves(idiot_temp):
     expected_freq_lo = [440.0 / 2 * i for i in idiot_temp]
     expected_freq_hi = [440.0     * i for i in idiot_temp]
     expected_freq = expected_freq_lo + expected_freq_hi
