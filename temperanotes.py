21acc53c6800fb15511733092cb5caa2a1b2168c
Davide Del Vento <davide.del.vento@gmail>
2016-11-19 13:59:16 -0700

    TDD: generating frequencies to make the test pass

--- temperanotes.py
+++ temperanotes.py
@@ -1,2 +1,7 @@
-def frequencies(temperament, low, high):
-    return []
+def frequencies(temperament, low, high, base_freq = 440.0):
+    freq = []
+    for octave in range(2, low + 2):
+        freq =  [(base_freq / octave) * note for note in temperament] + freq
+    for octave in range(1, high + 1):
+        freq += [(base_freq * octave) * note for note in temperament]
+    return freq
