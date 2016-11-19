8c8a3cedb5fcf2e4152896af299ee28973a6ee9f
Davide Del Vento <davide.del.vento@gmail>
2016-11-19 14:16:13 -0700

    TDD: the frequency scaling is exponential, not linear... making the test pass

--- temperanotes.py
+++ temperanotes.py
@@ -1,7 +1,7 @@
 def frequencies(temperament, low, high, base_freq = 440.0):
     freq = []
-    for octave in range(2, low + 2):
-        freq =  [(base_freq / octave) * note for note in temperament] + freq
-    for octave in range(1, high + 1):
-        freq += [(base_freq * octave) * note for note in temperament]
+    for octave in range(1, low + 1):
+        freq =  [(base_freq / 2 ** octave) * note for note in temperament] + freq
+    for octave in range(high):
+        freq += [(base_freq * 2 ** octave) * note for note in temperament]
     return freq
