866b63f7656d1572089526bbf111a3dde01a1bdb
Davide Del Vento <davide.del.vento@gmail>
2016-11-19 14:31:11 -0700

    TDD: equal temperaments, making the test pass easier than test itself

--- temperanotes.py
+++ temperanotes.py
@@ -5,3 +5,6 @@ def frequencies(temperament, octaves_low, octaves_high, base_freq = 440.0):
     for octave in range(octaves_high):
         freq += [(base_freq * 2 ** octave) * note for note in temperament]
     return freq
+
+def equal_temperament():
+    return [ 2 ** (i/12)  for i in range(12)]
