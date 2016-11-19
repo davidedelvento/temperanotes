6cd74fde23e7d68ec1dd92585b85f4b1ac5d6568
Davide Del Vento <davide.del.vento@gmail>
2016-11-19 14:20:31 -0700

    refactoring, just a more clear naming

--- temperanotes.py
+++ temperanotes.py
@@ -1,7 +1,7 @@
-def frequencies(temperament, low, high, base_freq = 440.0):
+def frequencies(temperament, octaves_low, octaves_high, base_freq = 440.0):
     freq = []
-    for octave in range(1, low + 1):
+    for octave in range(1, octaves_low + 1):
         freq =  [(base_freq / 2 ** octave) * note for note in temperament] + freq
-    for octave in range(high):
+    for octave in range(octaves_high):
         freq += [(base_freq * 2 ** octave) * note for note in temperament]
     return freq
--- test_temperanotes.py
+++ test_temperanotes.py
@@ -12,17 +12,17 @@ def test_two_octaves(idiot_temp):
     expected_freq_hi = [440.0     * i for i in idiot_temp]
     expected_freq = expected_freq_lo + expected_freq_hi
     assert len(expected_freq) == 24                                                      # obvious, but making sure no simply bugs in test itself
-    actual_freq = temperanotes.frequencies(temperament = idiot_temp, low = 1, high = 1)
+    actual_freq = temperanotes.frequencies(temperament = idiot_temp, octaves_low = 1, octaves_high = 1)
     assert actual_freq == expected_freq
 
 def test_normal_octave(idiot_temp):
     expected_freq = [440.0 * i for i in idiot_temp]
-    actual_freq = temperanotes.frequencies(temperament = idiot_temp, low = 0, high = 1)
+    actual_freq = temperanotes.frequencies(temperament = idiot_temp, octaves_low = 0, octaves_high = 1)
     assert actual_freq == expected_freq
 
 def test_lower_octave(idiot_temp):
     expected_freq = [440.0 / 2 * i for i in idiot_temp]
-    actual_freq = temperanotes.frequencies(temperament = idiot_temp, low = 1, high = 0)
+    actual_freq = temperanotes.frequencies(temperament = idiot_temp, octaves_low = 1, octaves_high = 0)
     assert actual_freq == expected_freq
 
 def test_four_octaves(idiot_temp):
@@ -32,6 +32,6 @@ def test_four_octaves(idiot_temp):
     expected_freq_hihi = [440.0 * 2 * i for i in idiot_temp]
     expected_freq = expected_freq_lolo + expected_freq_lo + expected_freq_hi + expected_freq_hihi
     assert len(expected_freq) == 48                                                      # obvious, but making sure no simply bugs in test itself
-    actual_freq = temperanotes.frequencies(temperament = idiot_temp, low = 2, high = 2)
+    actual_freq = temperanotes.frequencies(temperament = idiot_temp, octaves_low = 2, octaves_high = 2)
     assert actual_freq == expected_freq
 
